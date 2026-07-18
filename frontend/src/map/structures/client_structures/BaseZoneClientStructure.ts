bf901448ab8c405c98ae1b5a126e960128adebef
kimihipi <yoanholder@gmail.com>
2026-07-18 10:03:55 -0400

    fix: upstream changes

--- frontend/src/map/structures/client_structures/BaseZoneClientStructure.ts
+++ frontend/src/map/structures/client_structures/BaseZoneClientStructure.ts
@@ -175,6 +175,8 @@ abstract class BaseZoneClientStructure extends ClientStructure {
     }
 
     postProcess(): void {
+        this.isResizing = false;
+
         this.x0 = Math.round(this.x0);
         this.y0 = Math.round(this.y0);
 
